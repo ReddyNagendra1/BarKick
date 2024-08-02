4c2047240465c9c4cbf73bbde49e7d46fdb2df3f
Deryn Boscariol <derynb@rogers.com>
2024-08-02 04:35:06 -0400

    All base files (still need more relational functionality

---
+++ BarKick/Models/ViewModels/AddTeamToVenueViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BarKick.Models;
+using BarKick.Models.ViewModels;
+
+namespace BarKick.Models.ViewModels
+{
+    public class AddTeamToVenueViewModel
+    {
+        public VenueDto Venue { get; set; }
+        public IEnumerable<TeamDto> Teams { get; set; }
+    }
+}
